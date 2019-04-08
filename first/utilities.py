e8d7216c3af26ca9ceedc186b68b9ac214965092
Sam Chawla <sam.chawla26@gmail.com>
2019-04-08 20:07:50 +0530

    Added edit for header and login template added

--- first/utilities.py
+++ first/utilities.py
@@ -25,11 +25,20 @@ def updatetask(request):
 def updatecontent(request):
     updated_content = request.GET.get('updatedcontent')
     task_id = request.GET.get('taskid')
-    if Task.objects.filter(id=task_id).exists():
-        task = Task.objects.get(id=task_id)
-        task.content = updated_content
-        task.save()
-        return HttpResponse(json.dumps({'task': f"Task {task_id} Updated"}), content_type="application/json")
-    return HttpResponse(json.dumps({'task': f"Task {task_id}  Failed"}), content_type="application/json")
-    
+    is_title = request.GET.get('is_title')
+    if is_title=="true":
+        if Task.objects.filter(id=task_id).exists():
+            task = Task.objects.get(id=task_id)
+            task.title = updated_content.strip()
+            task.save()
+            return HttpResponse(json.dumps({'task': f"Task {task_id} Updated"}), content_type="application/json")
+        return HttpResponse(json.dumps({'task': f"Task {task_id}  Failed"}), content_type="application/json")
+    else:
+        if Task.objects.filter(id=task_id).exists():
+            task = Task.objects.get(id=task_id)
+            task.content = updated_content
+            task.save()
+            return HttpResponse(json.dumps({'task': f"Task {task_id} Updated"}), content_type="application/json")
+        return HttpResponse(json.dumps({'task': f"Task {task_id}  Failed"}), content_type="application/json")
+        
     
--- mylearnings/static/js/drag-drop.js
+++ mylearnings/static/js/drag-drop.js
@@ -1,31 +1,53 @@
 $(document).ready(function(){
 $(onPageLoad);
 
-$('.edit').click(function(){
-  $(this).hide();
-  $(this).parent().addClass('editable');
-  $(this).closest('.portlet').find('.portlet-content').attr('contenteditable', 'true');  
-  $(this).closest('.portlet').find('.save').show();
-  $(this).parent().find('.portlet-toggle').hide()
-  // .portlet-toggle
+// Saves text in 'portlet-content' on pressing ENTER
+$(".portlet-content").keypress(function (e) {
+  if (e.which == 13) {
+    updated_content = e.currentTarget.innerText
+    task_id = e.currentTarget.parentElement.id
+    onContentChange(task_id,updated_content,false);
+    $(e.currentTarget).removeAttr('contenteditable')
+  }
 });
 
-$('.save').click(function(){
-  $(this).hide();
-  $(this).parent().removeClass('editable');
-  $(this).closest('.portlet').find('.portlet-content').removeAttr('contenteditable');  
-  $(this).closest('.portlet').find('.edit').show();
-  // Extracting Text and task Id
-  var task_id = $(this).closest('.portlet').data('id')
-  var temp = $(this).closest('.portlet').find('.portlet-content')[0]
-  var updated_content = $(temp).text() 
-
-  onContentChange(task_id,updated_content);
-  $(this).parent().find('.portlet-toggle').show()
+// Saves text in 'portlet-header' on pressing ENTER
+$(".portlet-header").keypress(function (e) {
+  if (e.keyCode ==13 || e.which == 13) {
+    updated_title = e.currentTarget.innerText
+    task_id = e.currentTarget.parentElement.id
+    onContentChange(task_id,updated_title,true);
+    $(e.currentTarget).removeAttr('contenteditable')
+  }
 });
+
+
+// $('.edit').click(function(){
+//   $(this).hide();
+//   $(this).parent().addClass('editable');
+//   $(this).closest('.portlet').find('.portlet-content').attr('contenteditable', 'true');  
+//   $(this).closest('.portlet').find('.save').show();
+//   $(this).parent().find('.portlet-toggle').hide()
+//   // .portlet-toggle
+// });
+
+// $('.save').click(function(){
+//   $(this).hide();
+//   $(this).parent().removeClass('editable');
+//   $(this).closest('.portlet').find('.portlet-content').removeAttr('contenteditable');  
+//   $(this).closest('.portlet').find('.edit').show();
+//   // Extracting Text and task Id
+//   var task_id = $(this).closest('.portlet').data('id')
+//   var temp = $(this).closest('.portlet').find('.portlet-content')[0]
+//   var updated_content = $(temp).text() 
+
+//   onContentChange(task_id,updated_content);
+//   $(this).parent().find('.portlet-toggle').show()
+// });
 
 })
 
+// Call this function to update task status
 function onTaskChange(taskchange,task_id,order){
 $.ajax({
   type:"GET",
@@ -39,13 +61,27 @@ $.ajax({
   });
 }
 
-function onContentChange(task_id,updated_content){
+// To enable content editing
+function EditContent(ref_element){
+  console.log(ref_element)
+  $(ref_element).attr('contenteditable','true')
+  }
+
+// To enable header Editing
+function EditHeader(ref_element){
+  console.log(ref_element);
+  // debugger
+  $(ref_element).attr('contenteditable','true')
+  }
+
+// Call this function to change content or title of task
+function onContentChange(task_id,updated_content,title){
   $.ajax({
     type:"GET",
     cache:false,
     url:'/updatecontent',
     dataType: "json",
-    data:{taskid:task_id,updatedcontent:updated_content},    // multiple data sent using ajax
+    data:{taskid:task_id,updatedcontent:updated_content,is_title:title},    // multiple data sent using ajax
     success: function (result) {
       console.log(result['task'])
     }
@@ -53,13 +89,13 @@ function onContentChange(task_id,updated_content){
   }
 
 
-
+// Called on Page Load
 function onPageLoad()
 {
   $( ".column" ).sortable({
     connectWith: ".column",
     handle: ".portlet-header",
-    cancel: ".portlet-toggle",
+    cancel: ".contenteditable,.portlet-content,.portlet-header-content",
     start: function (event, ui) {
       ui.item.addClass('tilt');
     },
@@ -81,18 +117,19 @@ function onPageLoad()
   $( ".portlet" )
     .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
     .find( ".portlet-header" )
-      .addClass( "ui-widget-header ui-corner-all" )
-      .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+      .addClass( "ui-widget-header ui-corner-all" );
+      // .prepend( "<span ondblclick='EditContent(this)' class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
 
-  $( ".portlet-toggle" ).click(function() {
-    var icon = $( this );
-    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
-    if ($(icon).hasClass('ui-icon-plusthick')){
-        $(icon).closest(".portlet").find(".edit").hide()
-    }
-    else{
-      $(icon).closest(".portlet").find(".edit").show()
-    }
-  });
+  // $( ".portlet-toggle" ).click(function() {
+  //                                         });
+  //   var icon = $( this );
+  //   // icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+  //   icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+  //   if ($(icon).hasClass('ui-icon-plusthick')){
+  //       $(icon).closest(".portlet").find(".edit").hide()
+  //   }
+  //   else{
+  //     $(icon).closest(".portlet").find(".edit").show()
+  //   }
+  // });
 }
